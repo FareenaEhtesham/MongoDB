05b122026adc388cfe78bd8d91f8ec885b804f85
FareenaEhtesham <57524242+FareenaEhtesham@users.noreply.github.com>
2020-01-14 08:08:42 -0800

    Connect MongoDB to Nodejs

---
+++ sample.js
@@ -0,0 +1,17 @@
+var MongoClient =require('mongodb').MongoClient,format=require('util').format;
+
+
+
+MongoClient.connect('mongodb://127.0.0.1:27017',function(err ,db){
+    if(err){
+        throw err
+    }
+
+    else{
+        console.log('connected')
+    }
+
+    db.close();
+});
+
+//yeah connected
